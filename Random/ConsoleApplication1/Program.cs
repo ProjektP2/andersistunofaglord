ed83aa06ab47c55417aec4b584427539607cddd2
Mysjkin <iarogolo@gmail.com>
2016-03-01 10:05:52 +0100

    lol
    lol

--- Random/ConsoleApplication1/Program.cs
+++ Random/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace Random
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anders ist uno faglord!");
+            Console.WriteLine("Anders ist uno faglord!!!");
             Console.ReadKey();
         }
     }
